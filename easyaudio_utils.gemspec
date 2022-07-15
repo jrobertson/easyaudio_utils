a345370ccb99eed544ad3525f9492e0cf076425d
James Robertson <digital.robertson@gmail.com>
2022-07-15 08:52:28 +0100

    bug fix: *wav_to_ogg* is now working.

--- easyaudio_utils.gemspec
+++ easyaudio_utils.gemspec
@@ -1,17 +1,17 @@
 Gem::Specification.new do |s|
   s.name = 'easyaudio_utils'
-  s.version = '0.3.2'
+  s.version = '0.4.0'
   s.summary = 'A wrapper for various command-line audio utilities ' + 
       'under GNU/Linux.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/easyaudio_utils.rb']
-  s.add_runtime_dependency('c32', '~> 0.2', '>=0.2.0')  
+  s.add_runtime_dependency('c32', '~> 0.3', '>=0.3.0')
   s.add_runtime_dependency('wavtool', '~> 0.1', '>=0.1.0')
   s.add_runtime_dependency('ruby-mp3info', '~> 0.8', '>=0.8.10')
   s.add_runtime_dependency('ruby-ogginfo', '~> 0.7', '>=0.7.2')
   s.signing_key = '../privatekeys/easyaudio_utils.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/easyaudio_utils'
 end
--- lib/easyaudio_utils.rb
+++ lib/easyaudio_utils.rb
@@ -9,7 +9,7 @@ require 'ogginfo'
 # `apt-get install mplayer sox vorbis-tools ffmpeg
 
 # installing youtube-dl:
-# 
+#
 # `sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/bin/youtube-dl`
 #
 # `sudo chmod a+rx /usr/bin/youtube-dl`
@@ -20,7 +20,7 @@ require 'ogginfo'
 
 module CommandHelper
   using ColouredText
-  
+
   def list(a=@commands)
 
     format_command = ->(s) do
@@ -48,7 +48,7 @@ class EasyAudioUtils
 * concat_files # stiches wav files together
 * convert # converts a file from 1 format to another #wav #ogg
 * cut # cuts the audio into a new file as defined by start time and duration
-* duration # return the duration for a wav or ogg 
+* duration # return the duration for a wav or ogg
 * generate_silence # generates silence in a wav file
 * play # plays using mplayer
 * record # alias for capture_desktop
@@ -58,7 +58,7 @@ class EasyAudioUtils
 ".strip.lines.map {|x| x[/(?<=\* ).*/]}.sort
 
 
-  def initialize(audio_in=nil, audio_out='audio.wav', out: audio_out, 
+  def initialize(audio_in=nil, audio_out='audio.wav', out: audio_out,
                  working_dir: '/tmp')
 
     @file_in, @file_out, @working_dir = audio_in, out, working_dir
@@ -70,42 +70,42 @@ class EasyAudioUtils
   #
   def capture(show: false)
 
-    command = "rec -c 1 -r 8000 -t alsa default #{@file_out} " + 
+    command = "rec -c 1 -r 8000 -t alsa default #{@file_out} " +
         "silence 1 0.1 5% 5 1.0 5%"
     run command, show
 
   end
-  
-  def concat_files(files=[])
-    WavTool.new(out: @file_out, ).concat files
+
+  def concat_files(files=[], sample_rate: nil)
+    WavTool.new(out: @file_out, sample_rate: sample_rate).concat files
   end
-  
+
   alias concat concat_files
-  
+
   # convert either wav to ogg or ogg to wav
   #
   def convert()
-        
+
     if File.extname(@file_in) == '.ogg' then
-      ogg_to_wav() if File.extname(@file_out) == '.wav' 
+      ogg_to_wav() if File.extname(@file_out) == '.wav'
     else
-      wav_to_ogg() if File.extname(@file_out) == '.ogg' 
+      wav_to_ogg() if File.extname(@file_out) == '.ogg'
     end
-    
+
   end
-  
+
   # cut a section of audio and save it to file
   #
-  def cut(starttime, duration)    
-    
+  def cut(starttime, duration)
+
     command = "avconv -i %s -ss %s -t %s %s" % \
         [@file_in, starttime, duration, @file_out]
-    run command, show    
-    
+    run command, show
+
   end
-  
+
   def duration()
-    
+
     case File.extname(@file_in)
     when '.ogg'
       OggInfo.open(@file_in).length
@@ -113,71 +113,71 @@ class EasyAudioUtils
       WavTool.new().duration(@file_in)
     when '.mp3'
       Mp3Info.new(@file_in).length
-    end    
-    
+    end
+
   end
-  
+
   # silence duration in seconds
   #
   def generate_silence(duration)
     WavTool.new(out: @file_out).silence duration: duration
   end
-  
+
   alias record capture
-  
+
   def play(show: false)
     command = "mplayer #{@file_out}"
     run command, show
   end
-  
+
   # split by silence
   #
-  def split(show: false)    
+  def split(show: false)
     command = "sox -V3 #{@file_in} #{@file_out} silence -l  0  " +
         " 1 0.5 0.1% : newfile : restart"
     run command, show
   end
-  
+
   # volume increase or decrease
   #
-  def volume(amount=1.0, show: false)    
+  def volume(amount=1.0, show: false)
     command = "sox -v #{amount} #{@file_in} #{@file_out}"
     run command, show
-  end  
-  
+  end
+
   # Download and extract audio from a video on YouTube
   #
   # By default, Youtube-dl will save the audio in Ogg (opus) format.
   #
-  def youtube_dl(show: false)    
-    
+  def youtube_dl(show: false)
+
     command = "youtube-dl -x #{url=@file_in}"
     command += ' -o ' + @file_out if @file_out
-    run command, show    
+    run command, show
 
-  end  
+  end
 
 
   private
-  
-  
+
+
   def ogg_to_wav()
-    `oggdec #{@file_in} #{@file_out}`
-  end  
-  
-  def ogg_to_wav()
+    `oggdec #{@file_in} -o #{@file_out}`
+  end
+
+  def wav_to_ogg()
     `sox -V #{@file_in} #{@file_out}`
-  end   
+  end
 
   def run(command, show=false)
 
-    if show then 
+    if show then
       command
     else
       puts "Using ->" + command
       system command
     end
 
-  end  
+  end
 
 end
